4e631be68277a04f4c6fca3f4bbd665dc4d02fec
tranMinhTien2k3 <21013123@st.phenikaa-uni.edu.vn>
2024-01-16 16:44:09 +0700

    no change

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:appdemo/screens/home_screen.dart';
 void main() {
-  runApp(const MaterialApp(
-    home: MyApp(),
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Knowledge Sharing App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,31 +1,116 @@
 import 'package:flutter/material.dart';
-import 'package:appdemo/widgets/content_card.dart';
-import 'package:appdemo/screens/content_detail_screen.dart';
-import 'package:appdemo/screens/schedule_detail.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _isSearching = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 182, 218, 184),
       appBar: AppBar(
-        title: Text('Home Screen'),
+         title: _isSearching
+            ? TextField(
+                onChanged: (value) {
+                  // Xử lý sự kiện khi nội dung tìm kiếm thay đổi
+                },
+                decoration: const InputDecoration(
+                  hintText: 'Search...',
+                  border: InputBorder.none,
+                ),
+                style: const TextStyle(color: Colors.white),
+              )
+            : const Text('Home Screen'),
+        backgroundColor: Colors.blue,
+        actions:[
+           IconButton(
+            icon: _isSearching ? Icon(Icons.cancel) : Icon(Icons.search),
+            onPressed: () {
+              setState(() {
+                _isSearching = !_isSearching;
+              });
+            },
+          ),
+          
+        ]
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
           children: [
-            Text(
-              'Popular Content',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.green,
+              ), //BoxDecoration
+              child: UserAccountsDrawerHeader(
+                decoration: BoxDecoration(color: Colors.green),
+                accountName: Text(
+                  "Abhishek Mishra",
+                  style: TextStyle(fontSize: 18),
+                ),
+                accountEmail: Text("abhishekm977@gmail.com"),
+                currentAccountPictureSize: Size.square(50),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Color.fromARGB(255, 165, 255, 137),
+                  child: Text(
+                    "A",
+                    style: TextStyle(fontSize: 30.0, color: Colors.blue),
+                  ), //Text
+                ), //circleAvatar
+              ), //UserAccountDrawerHeader
+            ), //DrawerHeader
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text(' My Profile '),
+              onTap: () {
+                Navigator.pop(context);
+              },
             ),
-            SizedBox(height: 16),
-            
+            ListTile(
+              leading: const Icon(Icons.book),
+              title: const Text(' My Course '),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.workspace_premium),
+              title: const Text(' Go Premium '),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.video_label),
+              title: const Text(' Saved Videos '),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.edit),
+              title: const Text(' Edit Profile '),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('LogOut'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
           ],
         ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
       ),
     );
   }
